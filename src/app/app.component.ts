0af1439d6f59905dcf41f50364cc66a706d737b1
Vuducvi1999 <vuducvi20@gmail.com>
2021-03-16 23:27:08 +0700

    attribute binding bằng 2 cách: [src] = "value" hoặc src = "{{value}}"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'project3';
+  AnhTrenMang_1 = 'http://www.elle.vn/wp-content/uploads/2017/07/25/hinh-anh-dep-1.jpg'
+  HideAnh_1 = true
+  AnhTrenMang_2 = 'http://50mm.vn/wp-content/uploads/2017/11/windows_xp_bliss-wide-1024x640.jpg'
 }
