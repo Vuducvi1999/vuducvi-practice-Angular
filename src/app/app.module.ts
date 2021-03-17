1daf5f8b090096d31390224b7f45510a850de006
Vuducvi1999 <vuducvi20@gmail.com>
2021-03-17 16:08:02 +0700

    sử dụng @Input() trong Angular thay thế cho props trong React

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputSampleComponent } from './input-sample/input-sample.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputSampleComponent
   ],
   imports: [
     BrowserModule,
