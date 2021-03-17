6f6c07e066710e580156bb156df6711057715310
Vuducvi1999 <vuducvi20@gmail.com>
2021-03-17 15:30:30 +0700

    practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  addText = '';
+  txtContainer = [{ id: '1', txt: 'Default' }];
+  addItem(txt) {
+    this.txtContainer.push({ id: this.create_UUID(), txt });
+    this.addText = '';
+  }
+  deleteItem(id) {
+    this.txtContainer = this.txtContainer.filter((i) => i.id != id);
+  }
+  create_UUID() {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+      }
+    );
+    return uuid;
+  }
 }
