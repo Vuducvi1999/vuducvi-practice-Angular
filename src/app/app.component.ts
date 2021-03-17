35d1f78ae781d8da6df96c13dccce003a10c94ea
Vuducvi1999 <vuducvi20@gmail.com>
2021-03-17 14:54:42 +0700

    *ngIf và *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  renderP = true;
+  SubjectContainer = ["Angular","ASP.Net Core","Entity Framework"]
+  changeRenderP(){
+    this.renderP = !this.renderP;
+  }
 }
