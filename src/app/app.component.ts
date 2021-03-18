b74d2b8ffba9d38dcc231cc5bc0fdfd1431011da
Vuducvi1999 <vuducvi20@gmail.com>
2021-03-18 14:44:06 +0700

    first step to Reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  easyForm: FormGroup = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
+  onSubmit() {
+    console.log(this.easyForm);
+  }
 }
